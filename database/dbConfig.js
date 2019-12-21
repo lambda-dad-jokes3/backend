d72684c4347fd6ac77a26aeee591e23b295613e7
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2019-12-20 22:47:02 -0500

    changed knexfile and dbConfig

--- database/dbConfig.js
+++ database/dbConfig.js
@@ -2,6 +2,6 @@ const knex = require("knex");
 
 const knexConfig = require("../knexfile");
 
-const environment = process.env.DB || "development";
+const environment = process.env.DB || "production";
 
-module.exports = knex(knexConfig[environment]);
+module.exports = knex(knexConfig["development"]);
--- knexfile.js
+++ knexfile.js
@@ -5,25 +5,25 @@ const pg = require("pg");
 pg.defaults.ssl = true;
 
 module.exports = {
+  // development: {
+  //   client: "sqlite3",
+  //   useNullAsDefault: true,
+  //   connection: {
+  //     filename: "./database/jokes.db3"
+  //   },
+  //   migrations: {
+  //     directory: "./database/migrations"
+  //   },
+  //   seeds: {
+  //     directory: "./database/seeds"
+  //   },
+  //   pool: {
+  //     afterCreate: (conn, done) => {
+  //       conn.run("PRAGMA foreign_keys = ON", done);
+  //     }
+  //   }
+  // },
   development: {
-    client: "sqlite3",
-    useNullAsDefault: true,
-    connection: {
-      filename: "./database/jokes.db3"
-    },
-    migrations: {
-      directory: "./database/migrations"
-    },
-    seeds: {
-      directory: "./database/seeds"
-    },
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run("PRAGMA foreign_keys = ON", done);
-      }
-    }
-  },
-  production: {
     client: "pg",
     connection: process.env.DATABASE_URL,
     pool: {

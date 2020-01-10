aade3bee212f3244107e353b6de86adb9dbfb511
Brandon Davis <49970067+Bdavis90@users.noreply.github.com>
2020-01-09 19:36:47 -0500

    moved server.js into api folder and added server.spec.js for testing

---
+++ api/server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const server = express();
+const authorization = require("../auth/auth-middleware");
+
+const authRouter = require("../auth/authRoute");
+const jokesRouter = require("../jokesRouter/jokesRouter");
+
+server.use(morgan("dev"));
+server.use(helmet());
+server.use(cors());
+server.use(express.json());
+
+server.use("/user", authRouter);
+server.use("/jokes", authorization, jokesRouter);
+
+server.get("/", (req, res) => {
+  res.send("Welcome to dad jokes");
+  // res.status(200).json({ hello: "World!" });
+});
+
+module.exports = server;
